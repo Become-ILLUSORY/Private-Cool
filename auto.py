f9f984ead48034b64bd44c9f42917b0195fd47bf
镜花水月 <79624137+Become-ILLUSORY@users.noreply.github.com>
2025-10-05 21:40:19 +0800

    Update auto.py

--- auto.py
+++ auto.py
@@ -330,7 +330,7 @@ def main():
     # 插入站点（批量插入+单独插入自定义站点）
     SiteProcessor.insert_sites(original_data, new_sites)
     emby_feiniu_site = {
-        "ext": "eyJzZXJ2ZXIiOiAiaHR0cDovL215Z2Nucy5tb2JhaWVtYnkuc2l0ZTo3MDY5IiwidXNlcm5hbWUiOiAi6ZWc6Iqx5rC05pyIIiwicGFzc3dvcmQiOiAiMjA0MjE5ODE2Ny4uLiIsInVhIjogIllhbWJ5LzEuMC4yKEFuZHJvaWQpIn0=",
+        "ext": "eyJzZXJ2ZXIiOiAiaHR0cDovL215Z2Nucy5tb2JhaWVtYnkuc2l0ZTo3MDY5IiwidXNlcm5hbWUiOiAi6ZWc6Iqx5rC05pyIIiwicGFzc3dvcmQiOiAiMjA0MjE5ODE2Ny4uLiIsInVhIjogIllhbWJ5LzEuMC4yKEFuZHJvaWQpIiwiY29tbW9uQ29uZmlnIjogIiJ9",
         "filterable": 1,
         "quickSearch": 1,
         "name": "Emby墨云阁",
@@ -362,3 +362,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
